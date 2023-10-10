6d07b15858838f68ad4a2e6e7224b5d8253e488e
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-10 15:09:32 +0200

    added basic alert function in case of execution with no data

--- js/script.js
+++ js/script.js
@@ -18,3 +18,29 @@ MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
+
+//const namesur = document.getElementById('nome').value;
+//const km = document.getElementById('km').value;
+//const age = document.getElementById('eta').value;
+
+const generatebtn = document.querySelector('.generatebtn');
+const cancbtn = document.querySelector('.cancelbtn');
+
+/*
+console.dir(generatebtn);
+console.dir(cancbtn);
+*/
+
+generatebtn.addEventListener('click', 
+    function() {
+        const namesur = document.getElementById('nome').value;
+        const km = document.getElementById('km').value;
+        const age = document.getElementById('eta').value;
+        if (namesur === '' || km === '' || age === '') {
+            alert ('Inserisci dei dati corretti!')
+        } else {
+            
+        }
+
+    }
+);
